e1432c0c03a574cf56ab92addf01c4825c8e2512
Sudha Sravya s571487 <s571487@nwmissouri.edu>
2024-04-04 20:23:53 -0500

    lab

--- models/costumeController.js
+++ models/costumeController.js
@@ -17,17 +17,38 @@ exports.costume_create_post = async function(req, res) {
 };
 
 
-exports.costume_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id);
+exports.costume_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await Costume.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
-
 exports.costume_delete = function(req, res) {
     res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id);
 };
 
-exports.costume_update_put = function(req, res) {
-    res.send('NOT IMPLEMENTED: Costume update PUT' + req.params.id);
-};
+exports.costume_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await Costume.findById( req.params.id)
+    if(req.body.costume_type)
+    toUpdate.costume_type = req.body.costume_type;
+    if(req.body.cost) toUpdate.cost = req.body.cost;
+    if(req.body.size) toUpdate.size = req.body.size;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
 
 exports.costume_list = async function (req, res) {
     try {
