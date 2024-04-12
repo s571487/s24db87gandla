be8b0fe268ba3f511a547c070e463ccd2c66dc3e
Sudha Sravya s571487 <s571487@nwmissouri.edu>
2024-04-11 21:10:52 -0500

    Lab

--- app.js
+++ app.js
@@ -3,21 +3,54 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
+  })
+)
+
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
 mongoose = require('mongoose');
 mongoose.connect(connectionString);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var guitarRouter = require('./routes/guitar');
 var gridRouter = require('./routes/grid');
-var randomitemRouter=require('./routes/pick');
+var randomitemRouter = require('./routes/pick');
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 var app = express();
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.get('/grid', (req, res) => {
   let query = req.query;
   console.log(`Rows: ${query.rows}`);
@@ -42,20 +75,20 @@ app.use('/grid', gridRouter);
 app.use('/randomitem', randomitemRouter);
 app.get('/sudha', async (req, res) => {
   try {
-      const costumes = await Costume.find();
-      res.render('sudha', { costumes: costumes });
+    const costumes = await Costume.find();
+    res.render('sudha', { costumes: costumes });
   } catch (err) {
-      res.status(500).send(`Error fetching data: ${err}`);
+    res.status(500).send(`Error fetching data: ${err}`);
   }
 });
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
@@ -73,38 +106,38 @@ var Costume = require('./models/costume.js');
 
 
 async function recreateDB() {
-  
+
   await Costume.deleteMany();
 
   let instance1 = new Costume({
-      costume_type: "ghost",
-      size: 'large',
-      cost: 15.4
+    costume_type: "ghost",
+    size: 'large',
+    cost: 15.4
   });
 
   let instance2 = new Costume({
-      costume_type: "vampire",
-      size: 'medium',
-      cost: 20.0
+    costume_type: "vampire",
+    size: 'medium',
+    cost: 20.0
   });
 
   let instance3 = new Costume({
-      costume_type: "witch",
-      size: 'small',
-      cost: 12.5
+    costume_type: "witch",
+    size: 'small',
+    cost: 12.5
   });
 
   await instance1.save()
-      .then(doc => console.log("First object saved"))
-      .catch(err => console.error(err));
+    .then(doc => console.log("First object saved"))
+    .catch(err => console.error(err));
 
   await instance2.save()
-      .then(doc => console.log("Second object saved"))
-      .catch(err => console.error(err));
+    .then(doc => console.log("Second object saved"))
+    .catch(err => console.error(err));
 
   await instance3.save()
-      .then(doc => console.log("Third object saved"))
-      .catch(err => console.error(err));
+    .then(doc => console.log("Third object saved"))
+    .catch(err => console.error(err));
 }
 
 let reseed = true;
